4596ba9cba8b046dcc4520037ffa09835ed998a6
Shreyansh Singh Thakur <thakur.shreyansh11@gmail.com>
2025-07-01 01:02:49 +0530

    added signup page

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Login from "./pages/Login";
 import HomePage from "./pages/HomePage";
+import Signin from "./pages/Signin";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signin />} />
         </Routes>
       </main>
       <Footer />
---
+++ Frontend/src/pages/Signin.tsx
@@ -0,0 +1,81 @@
+import { useState } from "react";
+
+const Signin = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const submitHandler = async (e: React.FormEvent) => {
+    e.preventDefault(); // to stop browser from loading the whole page on submit
+    setError(null);
+    setLoading(true);
+
+    // create new account in database
+    // set local storage token and userId
+  };
+
+  return (
+    <>
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <div className="bg-white shadow-md rounded-lg px-8 pt-6 pb-8 w-full max-w-md">
+          <h2 className="text-2xl font-bold mb-6 text-center text-gray-800">
+            Create a new account
+          </h2>
+          <form onSubmit={submitHandler}>
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2">
+                Email Address
+              </label>
+              <input
+                className="shadow appearance-none border rounded px-3 py-2 text-gray-700 w-full leading-tight focus:outline-none focus:shadow-outline"
+                type="email"
+                placeholder="Your email"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+            <div className="mb-6">
+              <label className="block text-gray-700 text-sm font-bold mb-2">
+                Password
+              </label>
+              <input
+                className="shadow appearance-none border rounded px-3 py-2 text-gray-700 w-full leading-tight focus:outline-none focus:shadow-outline"
+                type="password"
+                placeholder="Your password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div className="flex items-center justify-between mb-4">
+              <button
+                className="bg-blue-500 hover:bg-blue-600 text-white font-bold px-4 py-2 rounded focus:outline-none focus:shadow-outline"
+                type="submit"
+                disabled={loading}
+              >
+                {loading ? "Signing in..." : "Sign in"}
+              </button>
+              <p className="text-gray-600 flex text-xs font-light">
+                Already have an account?
+                <a
+                  href="/login"
+                  className="text-blue-500 hover:text-blue-700 ml-1 font-semibold"
+                >
+                  Login
+                </a>
+              </p>
+            </div>
+            {error && (
+              <p className="text-red-500 text-xs italic mb-4">{error}</p>
+            )}
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Signin;
