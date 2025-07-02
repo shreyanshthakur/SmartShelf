ee7f20e485d246c03f6e9300d5924eb8189122f6
Shreyansh Singh Thakur <thakur.shreyansh11@gmail.com>
2025-07-03 01:15:46 +0530

    designed homepage

---
+++ Frontend/src/components/Item.tsx
@@ -0,0 +1,5 @@
+const Item = (itemImage: string, itemName: string, itemPrice: string) => {
+  return <div></div>;
+};
+
+export default Item;
--- Frontend/src/pages/HomePage.tsx
+++ Frontend/src/pages/HomePage.tsx
@@ -1,9 +1,18 @@
 function HomePage() {
   return (
     <div>
-      <div className="flex items-start justify-center min-h-screen bg-gray-100">
-        <div className="bg-white shadow-md rounded-lg px-8 pt-6 pb-8 w-full max-w-md flex justify-center">
-          Exclusive Offers
+      <div className="flex flex-col items-center bg-gray-100">
+        <div className="flex items-center justify-center bg-gray-50 shadow-lg rounded-lg py-4 w-full max-w-full text-center">
+          <div className="mr-2 items-start justify-start pr-6">🍔</div>
+          <div className="mr-2">Search 🔎</div>
+          <input className="h-8 border border-gray-700 rounded min-w-lg"></input>
+          <div className="ml-2">Filter</div>
+        </div>
+        <div className="text-center shadow-md rounded-lg px-8 pt-6 pb-8 w-full max-w-full h-50">
+          sliding images banner
+        </div>
+        <div className="grid-cols-6 text-center shadow-md rounded-lg px-8 pt-6 pb-8 w-full max-w-full h-50">
+          items view
         </div>
       </div>
     </div>
