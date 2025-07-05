ce7efe7ba295471f996563d73edbeec3d03de978
Shreyansh Singh Thakur <thakur.shreyansh11@gmail.com>
2025-07-06 00:40:59 +0530

    Connected to db

--- Backend/db.tsx
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-
-// Replace with your MongoDB connection string
-const mongoUri = "mongodb://localhost:27017/your-db-name";
-mongoose.connect(mongoUri);
--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const app = express();
+const { connectDB } = require("./connectDB");
+connectDB();
 const PORT = 5000;
 const cors = require("cors");
 app.use(cors());
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+app.get("api/v1/itemList", async (req, res) => {
+  try {
+    const items = await Item.find();
+    res.json(items);
+  } catch (err) {
+    res.status(500).json({ error: "Failed to fetch items from database" });
+  }
 });
 
 app.listen(PORT, () => {
