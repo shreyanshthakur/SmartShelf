1c551ceb542ed46d25f1172f65a5d679f23ca7c2
Shreyansh Singh Thakur <thakur.shreyansh11@gmail.com>
2025-11-02 04:03:49 +0530

    bugfix user signin issue resolved

--- Backend/src/auth/registerController.ts
+++ Backend/src/auth/registerController.ts
@@ -2,9 +2,11 @@ import { Request, Response } from "express";
 import { register } from "../services/AuthService";
 
 export const registerController = async (req: Request, res: Response) => {
+  console.log("Called register controller");
   try {
     const { email, password, firstName, lastName } = req.body;
-
+    console.log(req.body);
+    console.log(email, password, firstName, lastName);
     if (!email || !password || !firstName || !lastName) {
       return res.status(400).json({
         error: "Email and password are required",
@@ -16,15 +18,16 @@ export const registerController = async (req: Request, res: Response) => {
         error: "Password must be at least 6 characters long",
       });
     }
-
+    console.log("Before calling register");
     const result = await register({ email, password, firstName, lastName });
-
+    console.log("after calling register");
     if (!result.success || !result.user) {
+      console.log("[DEBUG] 400 status");
       return res.status(400).json({
         error: result.message,
       });
     }
-
+    console.log("[DEBUG] 201 status");
     res.status(201).json({
       status: true,
       message: result.message,
