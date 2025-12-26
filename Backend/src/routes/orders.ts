6b45f753a41754f8067cea09857b0b018178a00c
Shreyansh Singh Thakur <thakur.shreyansh11@gmail.com>
2025-12-26 18:37:11 +0530

    added payment route and updated createOrderController

--- Backend/src/routes/orders.ts
+++ Backend/src/routes/orders.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
 import { authenticateToken } from "../middleware/authenticateToken";
 import { createOrderController } from "../orders/createOrderController";
+import { createPaymentIntentController } from "../orders/createPaymentIntentController";
 
 const router = Router();
 
+router.post(
+  "/create-payment-intent",
+  authenticateToken,
+  createPaymentIntentController
+);
 router.post("/order", authenticateToken, createOrderController);
 
 export default router;
